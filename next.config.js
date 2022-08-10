63ea303b89b2a7949729e8540da18c64a456e850
Jan Schering <j.schering@web.de>
2022-08-10 21:59:50 +0200

    syntax highlighting in-browser

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const isProd = process.env.NODE_ENV.trim() === "production";
 console.log(process.env.NODE_ENV.trim());
 
 const nextConfig = require("@next/mdx")({
+  options: {
+    providerImportSource: "@mdx-js/react",
+    remarkPlugins: [require("remark-prism")],
+  },
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
